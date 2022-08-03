697c4e6ee13fb3522ca864c93bca8bdaa540f235
Mohammad <hasaeen3252@gmail.com>
2022-08-03 11:02:57 +0300

    just remembered where the file is

--- week-5/mongoose/mongoose Exercise/api.js
+++ week-5/mongoose/mongoose Exercise/api.js
@@ -8,5 +8,27 @@ router.get('/people', function (req, res) {
         res.send(people)
     })
 })
+router.post('/person', function (req, res) {
+    let p1 = new Person(req.body)
+    p1.save()
+    res.end()
+})
 
+router.put('/person/:id', function (req, res) {
+    Person.findById(req.params.id, function (err, person) {
+        person.age = 80
+        person.save()
+    })
+    res.end()
+})
+router.delete('/apocalypse', function (req, res) {
+    Person.find({}, function (err, person) {
+        person.forEach(element => {
+            element.remove(function (err) {
+                console.log(err)
+            })
+        })
+        res.end()
+    })
+})
 module.exports = router
